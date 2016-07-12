326dddbc28cc2f430d6c5dd04b6c9a270d23b548
Abuk <abukcz@gmail.com>
2016-07-12 22:14:23 +0200

    switch default dpi to hdpi so mako will look like a normal phone and not like a phone for senior people lol

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ DEVICE_PACKAGE_OVERLAYS := device/lge/mako/overlay
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 PRODUCT_PACKAGES := \
     libwpa_client \
